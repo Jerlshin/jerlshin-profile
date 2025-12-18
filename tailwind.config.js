3f298fbc9064a027bea6d1c1cebc6a586e367066
Jerlshin <jerlshin.official008@gmail.com>
2025-12-19 00:50:00 +0530

    dark mode fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'selector', // THIS FIXES THE ISSUE by forcing class-based toggling
   theme: {
     extend: {},
   },
